86f38af0748626a6c75b9b43d40babe0adacf230
JaveriaShakoor <javeriashakoor9@gmail.com>
2025-03-19 00:25:08 +0500

    Initialize Django project structure

---
+++ url_shortener_api/urls.py
@@ -0,0 +1,27 @@
+"""
+URL configuration for url_shortener_api project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from api import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('shorten/', views.CreateShortURL.as_view()),  # POST (Create)
+    path('shorten/<str:short_code>/', views.ShortURLView.as_view()),  # GET, PUT, DELETE
+    path('shorten/<str:short_code>/stats/', views.GetURLStats.as_view()),  # GET (Statistics)
+
+]
